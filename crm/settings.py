713d21ddd4e020836b060bbdebcfd1ae6ba487cb
Aldash <aldash@easyuni.com>
2014-01-03 10:02:01 +0800

    Changed DB settings to PostgreSQL

--- crm/settings.py
+++ crm/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'onekloud.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crm',
+        'USER': 'crmuser',
+        'PASSWORD': 'vivendi89',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
