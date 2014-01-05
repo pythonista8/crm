c6903ab33204e7bf9065eb0e683ee1ed3c449bf4
Aldash Biybosunov <pythonista8@gmail.com>
2014-01-05 16:38:14 +0800

    Removed admin from installed apps

--- crm/settings.py
+++ crm/settings.py
@@ -38,7 +38,6 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # Django application definition
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
