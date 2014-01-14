d179f5b69384e36637aad42ad4f24c6dabf8d40f
Aldash Biybosunov <pythonista8@gmail.com>
2014-01-14 19:11:28 +0800

    Email activation link

--- crm/apps/accounts/views.py
+++ crm/apps/accounts/views.py
@@ -14,11 +14,13 @@ def login_form(request):
     success_url = reverse('events:index')
     if request.user.is_authenticated():
         return redirect(success_url)
+
     email = None
     if request.method == 'POST':
         email = request.POST['email']
         passw = request.POST['password']
         user = authenticate(email=email, password=passw)
+
         if user is not None:
             if user.is_active:
                 login(request, user)
@@ -31,32 +33,39 @@ def login_form(request):
                                         "administrator")
         else:
             messages.error(request, "Your email or password is incorrect.")
+
     if email is not None:
         form = LoginForm(initial=dict(email=email))
     else:
         form = LoginForm()
+
     return render(request, 'accounts/login_form.html', dict(form=form))
 
 
-def activate_and_create_user(request):
+def activate_trial(request):
     if request.method == 'GET':
         reqhash = request.GET['key']
         email = request.GET['email']
-        password = '{alg}{hash}'.format(alg='pbkdf2_sha256$12000$',
-                                        hash=request.GET['pw'])
-        cname = request.GET['cname']
+        cname = request.GET['company']
 
-        key = '{key}{email}'.format(key=settings.ACTIVATION_KEY,
-                                    email=email).encode('utf8')
-        hash_ = hashlib.md5(key).hexdigest()
-        if reqhash != hash_:
-            return http.HttpResponseForbidden()
+        # Email must be unique.
+        try:
+            User.objects.get(email=email)
+        except User.DoesNotExist:
+            key = '{key}{email}{company}'.format(
+                key=settings.ACTIVATION_SALT, email=email, company=cname
+            ).encode('utf8')
 
-        company = Company.objects.create(name=cname)
-        user = User.objects.create_user(email, None, company=company)
-        user.password = password  # Save hashed password, not plain-text
-        user.save()
-        messages.success(request, "You may now sign in with your email and "
-                                  "password.")
+            hash_ = hashlib.md5(key).hexdigest()
+            if reqhash != hash_:
+                return http.HttpResponseForbidden()
+
+            company = Company.objects.create(name=cname)
+            User.objects.create_user(email, 'demo', company=company)
+            messages.success(request, "You may now sign in with your email. "
+                                      "Password is `demo`.")
+        else:
+            messages.info(request, "You are already registered.")
         return redirect('accounts:login')
+
     return http.HttpResponseNotAllowed(['POST'])
