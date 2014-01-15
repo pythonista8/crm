9a07e3e3e2b41eaceff3af62c1608c4a432af841
Aldash <aldash@easyuni.com>
2014-01-15 13:30:10 +0800

    Update customer form.

--- crm/apps/customers/forms.py
+++ crm/apps/customers/forms.py
@@ -4,6 +4,11 @@ from apps.customers.models import Customer
 
 
 class CustomerForm(forms.ModelForm):
+    website = forms.URLField(widget=forms.TextInput, required=False)
+    facebook = forms.URLField(widget=forms.TextInput, required=False)
+    twitter = forms.URLField(widget=forms.TextInput, required=False)
+    linkedin = forms.URLField(widget=forms.TextInput, required=False)
+
     class Meta:
         model = Customer
         exclude = ('date_created', 'date_modified', 'user')
