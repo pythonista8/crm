12dcecb45bbb8c025cf62b411535290d6fdbab70
Aldash <aldash@easyuni.com>
2014-01-16 12:18:39 +0800

    Reports and phone format

--- crm/lib/format.py
+++ crm/lib/format.py
@@ -1,14 +1,11 @@
+"""Functions to work with stings."""
+
 
 def format_phone(string):
-    """Return nicely formatted phone number. If invalid characters 
+    """Return nicely formatted phone number. If invalid characters
     are found, omit them."""
     res = ''
     for i in list(string):
         if i.isdigit() or i.lower() == 'x':
             res += i
-    if res.startswith('0'):
-        if res.startswith('00'):
-            res = res[2:]
-        else:
-            res = res[1:]
     return res
