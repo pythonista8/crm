d3788ceab5c5ba74136dd37be5fe5e80a2f468a8
Aldash Biybosunov <pythonista8@gmail.com>
2014-01-17 17:39:59 +0800

    C:/Program Files/Git/events/ alias for events app.

--- crm/urls.py
+++ crm/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    url(r'^', include('apps.events.urls', namespace='events')),
+    url(r'^events/', include('apps.events.urls', namespace='events')),
     url(r'^auth/', include('apps.accounts.urls', namespace='accounts')),
     url(r'^customers/', include('apps.customers.urls', namespace='customers')),
     url(r'^reports/', include('apps.reports.urls', namespace='reports')),
