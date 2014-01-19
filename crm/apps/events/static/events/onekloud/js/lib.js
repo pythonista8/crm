d052d631e1f2fe40a46639843da68167b53cca1a
Aldash Biybosunov <pythonista8@gmail.com>
2014-01-19 15:39:18 +0800

    Added Follow-Up form

--- crm/apps/events/static/events/onekloud/js/lib.js
+++ crm/apps/events/static/events/onekloud/js/lib.js
@@ -1,6 +1,8 @@
 // Display minutes as two digit value.
 function fixMinutesDisplay() {
   var minsInput = $('#duration input[type="number"]').last();
-  var minsInputVal = minsInput.val();
-  if (minsInputVal.length == 1) minsInput.val('0' + minsInputVal);
+  if (minsInput.length) {
+    var minsInputVal = minsInput.val();
+    if (minsInputVal.length == 1) minsInput.val('0' + minsInputVal);
+  }
 }
