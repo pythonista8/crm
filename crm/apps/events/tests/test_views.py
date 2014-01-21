75a84eb9267e25433377b4f9a6e298549f21cd41
Aldash <aldash@easyuni.com>
2014-01-21 10:42:32 +0800

    Fix view tests.

--- crm/apps/events/tests/test_views.py
+++ crm/apps/events/tests/test_views.py
@@ -22,11 +22,13 @@ class IndexTest(test.TestCase):
 
     def test_post_empty(self):
         resp = self.client.post(reverse('events:index'), data=dict())
-        self.assertFormError(resp, 'event_form', 'text', 'This field is required.')
+        self.assertFormError(resp, 'event_form', 'text', 'This field is '
+                                                         'required.')
 
     def test_post_invalid(self):
         resp = self.client.post(reverse('events:index'), data=dict(text=5))
-        self.assertFormError(resp, 'event_form', 'text', 'Could not obtain date. Please add.')
+        self.assertFormError(resp, 'event_form', 'text', 'Could not obtain '
+                                                         'date. Please add.')
 
 
 class EventContextMixinTest(test.TestCase):
@@ -57,8 +59,7 @@ class MeetingUpdateTest(test.TestCase):
         setattr(self.view, 'object', self.object)
 
     def test_get(self):
-        resp = self.client.get(reverse('events:edit-meeting',
-                                       kwargs={'pk': self.object.pk}))
+        resp = self.view.get(self.request)
         self.assertEqual(resp.status_code, 200)
 
     def test_attrs(self):
@@ -82,8 +83,7 @@ class FollowUpUpdateTest(test.TestCase):
         setattr(self.view, 'object', self.object)
 
     def test_get(self):
-        resp = self.client.get(reverse('events:edit-followup',
-                                       kwargs={'pk': self.object.pk}))
+        resp = self.view.get(self.request)
         self.assertEqual(resp.status_code, 200)
 
     def test_attrs(self):
