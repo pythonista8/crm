351e9e27d983116637140df3cf8ff6eff1bf998f
Aldash <aldash@easyuni.com>
2014-01-21 10:54:34 +0800

    Fix context processor.

--- crm/apps/accounts/context_processors.py
+++ crm/apps/accounts/context_processors.py
@@ -2,9 +2,11 @@
 
 
 def users(request):
-    ctx = dict()
-    # Trial user?
-    ctx['TRIAL'] = True if request.user.is_trial else False
-    # Head of Department?
-    ctx['IS_HEAD'] = True if request.user.is_head else False
+    ctx = dict(TRIAL=False, IS_HEAD=False)
+    user = request.user
+    if user.is_authenticated():
+        if user.is_trial:
+            ctx['TRIAL'] = True  # Trial user
+        if user.is_head:
+            ctx['IS_HEAD'] = True  # Head of Department
     return ctx
