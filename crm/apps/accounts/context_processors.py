552e9bf869eed7d5625372cf8cb708d46a1ab159
Aldash Biybosunov <pythonista8@gmail.com>
2014-01-25 16:19:14 +0800

    Restructure is_trial field into several subscription plans

--- crm/apps/accounts/context_processors.py
+++ crm/apps/accounts/context_processors.py
@@ -2,11 +2,11 @@
 
 
 def users(request):
-    ctx = dict(TRIAL=False, IS_HEAD=False)
+    ctx = dict()
     user = request.user
     if user.is_authenticated():
-        if user.is_trial:
-            ctx['TRIAL'] = True  # Trial user
-        if user.is_head:
-            ctx['IS_HEAD'] = True  # Head of Department
+        # Trial user
+        ctx['IS_TRIAL'] = True if user.is_trial else False
+        # Head of Department
+        ctx['IS_HEAD'] = True if user.is_head else False
     return ctx
--- crm/apps/accounts/models.py
+++ crm/apps/accounts/models.py
@@ -60,8 +60,20 @@ class User(AbstractBaseUser, PermissionsMixin):
     phone = models.CharField(max_length=50, blank=True)
     is_active = models.BooleanField(default=True)
 
-    # Billing Info.
-    is_trial = models.BooleanField(default=True)
+    # Subscription Plan Info.
+    TRIAL = 'trial'
+    INDIVIDUAL = 'individual'
+    COMPANY = 'company'
+    ENTERPRISE = 'enterprise'
+    SUBSCRIPTION_PLAN_CHOICES = (
+        (TRIAL, "Free Trial"),
+        (INDIVIDUAL, "Individual"),
+        (COMPANY, "Company"),
+        (ENTERPRISE, "Enterprise"),
+    )
+
+    subscription_plan = models.CharField(
+        max_length=100, choices=SUBSCRIPTION_PLAN_CHOICES, default=TRIAL)
 
     # Company Info.
     company = models.ForeignKey(Company, related_name='users')
@@ -100,33 +112,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         """Sends an email to this User."""
         send_mail(subject, message, from_email, [self.email])
 
-    def get_profile(self):
-        """Returns site-specific profile for this user. Raises
-        SiteProfileNotAvailable if this site does not allow profiles.
-        """
-        warnings.warn("The use of AUTH_PROFILE_MODULE to define user profiles "
-                      "has been deprecated.", DeprecationWarning, stacklevel=2)
-        if not hasattr(self, '_profile_cache'):
-            from django.conf import settings
-            if not getattr(settings, 'AUTH_PROFILE_MODULE', False):
-                raise SiteProfileNotAvailable(
-                    'You need to set AUTH_PROFILE_MODULE in your project '
-                    'settings')
-            try:
-                app_label, model_name = settings.AUTH_PROFILE_MODULE.split('.')
-            except ValueError:
-                raise SiteProfileNotAvailable(
-                    'app_label and model_name should be separated by a dot in '
-                    'the AUTH_PROFILE_MODULE setting')
-            try:
-                model = models.get_model(app_label, model_name)
-                if model is None:
-                    raise SiteProfileNotAvailable(
-                        'Unable to load the profile model, check '
-                        'AUTH_PROFILE_MODULE in your project settings')
-                self._profile_cache = model._default_manager.using(
-                    self._state.db).get(user__id__exact=self.id)
-                self._profile_cache.user = self
-            except (ImportError, ImproperlyConfigured):
-                raise SiteProfileNotAvailable
-        return self._profile_cache
+    @property
+    def is_trial(self):
+        if self.subscription_plan == self.TRIAL:
+            return True
+        return False
